71552daebbc99e98767915f644e50b0f13e8bd5e
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-13 19:39:47 +0300

    added redux to application

--- server/index.js
+++ server/index.js
@@ -9,12 +9,13 @@ const app = express()
 dotenv.config()
 
 // Express middleware to connect to application
-// Posts routes are at 5000/posts
-app.use('/posts', postRoutes)
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
+
+// Posts routes are at 5000/posts
+app.use('/posts', postRoutes)
 
 // Connect to MongoDB
 const CONNECTION_URL = ''
