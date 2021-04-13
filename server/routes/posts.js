5f75dec40e3f5671bda21669194121e7035f18b1
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-13 16:13:59 +0300

    added logic to get and create posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,13 @@
 import express from 'express'
 
-import { getPosts } from '../controllers/posts.js'
+import { getPosts, createPosts } from '../controllers/posts.js'
 
 // Set up router
 const router = express.Router()
 
 // Routes
 router.get('/', getPosts)
+router.post('/', createPosts)
 
 // Export route
 export default router
