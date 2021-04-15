12089583291577c0841ba5c6891f7caf30b3aa80
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-15 16:02:59 +0300

    added like functionality

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -8,7 +8,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import defaultPicture from '../../../assets/pexels-karolina-grabowska-4194857.jpg'
 import useStyles from './styles'
 import { useDispatch } from 'react-redux'
-import { deletePost } from '../../../actions/posts'
+import { deletePost, likePost } from '../../../actions/posts'
 
 const Post = ({ post, setCurrentId }) => {
 	const classes = useStyles()
@@ -40,7 +40,7 @@ const Post = ({ post, setCurrentId }) => {
 				</Typography>
 			</CardContent>
 			<CardActions className={classes.cardActions}>
-				<Button size='small' color='primary' onClick={() => {}}>
+				<Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
 					<ThumbUpAltIcon fontSize='small' />
 					Like
 					{post.likeCount}
