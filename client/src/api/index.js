5cb42a9398feff98780f9bb8f6f07430e3af40d6
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-17 21:47:19 +0300

    added axios interceptor and fixed typo in middleware

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,14 @@ import axios from 'axios'
 
 const API = axios.create({ baseURL: 'http://localhost:5000/' })
 
+// Send token back to backend to verify logging
+API.interceptors.request.use((req) => {
+	if (localStorage.getItem('profile')) {
+		req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+	}
+	return req
+})
+
 // https://immo-post-project.herokuapp.com
 
 export const fetchPosts = () => API.get('/posts')
