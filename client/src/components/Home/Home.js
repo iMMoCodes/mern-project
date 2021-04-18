b436a36aedf56687f005b39e4c9963849a7178aa
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-18 13:27:02 +0300

    made app responsive also on tablet

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -19,10 +19,10 @@ const Home = () => {
 		<Grow in>
 			<Container>
 				<Grid className={classes.mainContainer} container justify='space-between' alignItems='stretch' spacing={3}>
-					<Grid item xs={12} sm={7}>
+					<Grid item xs={12} sm={12} md={7}>
 						<Posts setCurrentId={setCurrentId} />
 					</Grid>
-					<Grid item xs={12} sm={4}>
+					<Grid item xs={12} sm={12} md={4}>
 						<Form currentId={currentId} setCurrentId={setCurrentId} />
 					</Grid>
 				</Grid>
