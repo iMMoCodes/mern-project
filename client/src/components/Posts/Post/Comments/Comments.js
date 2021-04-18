3997a42cfe5600df1598d90e57907015d366d3f9
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-19 02:37:28 +0300

    styled comment section

--- client/src/components/Posts/Post/Comments/Comments.js
+++ client/src/components/Posts/Post/Comments/Comments.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { Card, Typography, TextField, CardContent, Button } from '@material-ui/core'
 
 import useStyles from './styles'
 
 const Comments = () => {
 	const classes = useStyles()
 	return (
-		<div>
-			<h1>COMMENTS</h1>
-		</div>
+		<Card className={classes.card}>
+			<Typography className={classes.title} variant='h5' gutterBottom>
+				Comments
+			</Typography>
+			<CardContent>
+				<Typography variant='body2' color='textSecondary' component='p'>
+					test comments
+				</Typography>
+			</CardContent>
+			<div className={classes.sendComments}>
+				<TextField className={classes.commentField} name='comment' variant='outlined' label='Comment' />
+				<Button className={classes.sendButton} variant='contained' size='small' color='primary'>
+					Send
+				</Button>
+			</div>
+		</Card>
 	)
 }
 
--- client/src/components/Posts/Post/Comments/styles.js
+++ client/src/components/Posts/Post/Comments/styles.js
@@ -1,3 +1,26 @@
 import { makeStyles } from '@material-ui/core/styles'
 
-export default makeStyles({})
+export default makeStyles({
+	title: {
+		margin: '0 auto',
+		padding: '0 16px',
+	},
+	card: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		borderRadius: '15px',
+		height: '100%',
+		position: 'relative',
+	},
+	sendComments: {
+		display: 'flex',
+		padding: '0 2px',
+	},
+	commentField: {
+		flex: '5',
+	},
+	sendButton: {
+		flex: '1',
+	},
+})
